55ad274e7c550ef46f73e394ec1b740117f38623
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2018-11-12 20:47:36 +0200

    third week

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -1,68 +1,62 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity, Image,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity, Image,
 } from 'react-native';
 
-class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.doSearch = this.doSearch.bind(this)
-    }
+const HomeScreen = (props) => {
+  const doSearch = () => {
+    props.navigation.navigate('Results');
+  };
 
-    doSearch() {
-        this.props.navigation.navigate('Results');
-    }
+  return (
+      <View>
+        <Image
+            style={styles.image}
+            source={{uri: 'http://placekitten.com/640/480'}}
+        />
+        <TextInput
+            style={styles.kentta}
+            placeholder={'Search for TV show'}
+            value={props.text}
+        />
+        <TouchableOpacity style={styles.container} onPress={doSearch}>
+          <Text style={styles.nappi}>Search</Text>
+        </TouchableOpacity>
+      </View>);
 
-    render() {
-        return (
-            <View>
-                <Image
-                    style={styles.image}
-                    source={{uri: 'http://placekitten.com/640/480'}}
-                />
-                <TextInput
-                    style={styles.kentta}
-                    placeholder={'Search for TV show'}
-                    value={this.props.text}
-                />
-                <TouchableOpacity style={styles.container} onPress={this.doSearch}>
-                    <Text style={styles.nappi}>Search</Text>
-                </TouchableOpacity>
-            </View>);
-    }
-}
+};
 
 const styles = StyleSheet.create({
-    kentta: {
-        height: 40,
-        borderColor: '#aaa',
-        borderWidth: 1,
-        margin: 8,
-        padding: 10,
-        borderRadius: 3,
-    },
-    container: {
-        alignItems: 'center',
-        padding: 10,
-    },
-    nappi: {
-        fontSize: 18,
-        backgroundColor: 'blue',
-        textAlign: 'center',
-        width: 150,
-        padding: 20,
-        color: 'white',
-        borderRadius: 8,
-        overflow: 'hidden',
-    },
-    image: {
-        alignSelf: 'stretch',
-        height: 320,
-    },
+  kentta: {
+    height: 40,
+    borderColor: '#aaa',
+    borderWidth: 1,
+    margin: 8,
+    padding: 10,
+    borderRadius: 3,
+  },
+  container: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  nappi: {
+    fontSize: 18,
+    backgroundColor: 'blue',
+    textAlign: 'center',
+    width: 150,
+    padding: 20,
+    color: 'white',
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  image: {
+    alignSelf: 'stretch',
+    height: 320,
+  },
 });
 
 export default HomeScreen;
--- containers/ResultScreen.js
+++ containers/ResultScreen.js
@@ -2,22 +2,15 @@ import React, {Component} from 'react';
 import ArticleList from '../components/ArticleList';
 import {StyleSheet} from 'react-native';
 
-class ResultScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.showSingle = this.showSingle.bind(this);
-  }
-
-  showSingle() {
-    this.props.navigation.navigate('Show');
+const ResultScreen = (props) => {
+  const showSingle = () => {
+    props.navigation.navigate('Show');
   };
 
-  render() {
-    return (
-        <ArticleList style={styles.list} showSingle={this.showSingle}/>
-    );
+  return (
+      <ArticleList style={styles.list} showSingle={this.showSingle}/>
+  );
 
-  }
 };
 
 const styles = StyleSheet.create({
